709bf5cbde4c830e946f3ddd996a3964b217e1ab
Omar <oab102@hotmail.com>
2023-01-15 11:39:26 -0500

    Change loading behavior

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,29 @@ import type { AppProps } from 'next/app'
 import Layout from '../components/Layout';
 import GlobalStyles from '../styles/GlobalStyles';
 import AppContext from '../context'
+import { useState, useEffect } from 'react'
 
 
 function MyApp(props: AppProps<{}>) {
-    const { Component, pageProps } = props  
-    
-    return(
-      <>
+  const { Component, pageProps } = props;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [])
+  return (
+    <>
       <GlobalStyles />
       <AppContext>
-      <Layout>
-      <Component {...pageProps} />
-      </Layout>
+        {isLoading ?
+          "" :
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        }
       </AppContext>
-      </>
-    )
+    </>
+  )
 }
 
 export default MyApp;
